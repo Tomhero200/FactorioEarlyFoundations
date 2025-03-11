500baf091a7e34c382c00a2d1f74c817b36701fe
Tomhero200 <tomhero200@merz-adendorf.de>
2025-03-11 17:00:35 +0100

    Not working

--- data-update.lua
+++ data-update.lua
@@ -1,7 +1,4 @@
 --data-update.lua
-
-if space_age then
     if data.raw.technology["space-science-pack"] then
         data.raw.technology["foundation"].prerequisites = {"space-science-pack"}
     end
-end
